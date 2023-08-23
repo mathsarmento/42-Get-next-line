010fecba1d146a8fd544e871b3d25b2595c86a72
Matheus Sarmento <matheus.sarmento@orhganiza.com.br>
2023-08-23 15:50:42 -0300

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -22,9 +22,7 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE < 1 || BUFFER_SIZE > __INT_MAX__)
 		return (NULL);
-	printf("ENTROU NA FUNÇÃO GNL\n");
 	line = readfile(line, save, fd);
-	printf("SAIU DA FUNC READ LINE\n");
 	return (line);
 }
 
@@ -51,17 +49,17 @@ static char *readfile(char *line, char *save, int fd)
 	{
 		if(!line)
 			line = ft_calloc(1, 1);
-		printf("ENTROU NO WHILE\n");
 		aux_line = copyread(line, save);
 		free(line);
 		line = ft_substr(aux_line, 0, ft_strlen(aux_line));
 		if (line[ft_strlen(line) - 1] != '\n')
 			break;
 		free(save);
+		save = ft_calloc(BUFFER_SIZE + 1, 1);
 		free(aux_line);
+		printf("NUM => %li\n", num);
 		num = read(fd, save, BUFFER_SIZE);
 	}
-	printf("NÃO ENTROU NO WHILE\n");
 	return (line);
 }
 
@@ -71,7 +69,6 @@ static char	*copyread(char *line, char *save)
 	char	*aux_cpy;
 	size_t	findbackspace;
 
-	printf("ENTROU NA FUNÇÃO copyread\n");
 	findbackspace = 0;
 	while(save[findbackspace] != '\n')
 	{
@@ -82,11 +79,8 @@ static char	*copyread(char *line, char *save)
 		}
 		findbackspace++;
 	}
-	printf("ACHOU O \\n \n");
 	aux_cpy = ft_substr(save, 0, findbackspace + 1);
-	printf("AUX CPY => %s", aux_cpy);
 	cpy = ft_strjoin(line, aux_cpy);
-	printf("CPY => %s", cpy);
 	free (aux_cpy);
 	return (cpy);
 }
@@ -100,7 +94,7 @@ int	main(void)
 	fd = open("teste", O_RDONLY);
 	str = get_next_line(fd);
 	str2 = get_next_line(fd);
-	printf("\n\nFIRST LINE => %s\n\n", str);
+	printf("\n\nFIRST LINE => %s", str);
 	printf("2 LINE => %s\n\n", str2);
 	close (fd);
 }
